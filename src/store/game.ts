f80d2b18017f63b6d5b2b6879c7443dfe2fc657c
favouritetasty <19307110112@fudan.edu.cn>
2025-04-30 13:52:05 +0800

    feat: 选项点击

--- src/store/game.ts
+++ src/store/game.ts
@@ -4,10 +4,14 @@ export interface GameModel {
     isCardAnimating: boolean;
     isChoiceAnimating: boolean;
     currentCard: number;
+    exitDirection: "left" | "right";
+    trigger: boolean;
 }
 
 export const $Game = model<GameModel>("GAME", {
     isCardAnimating: false,
     isChoiceAnimating: false,
     currentCard: 0,
+    exitDirection: "right",
+    trigger: false,
 });
