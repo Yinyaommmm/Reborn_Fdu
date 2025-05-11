ff8b2ffa250b9f114fc297ac18209fa0635f278c
favouritetasty <19307110112@fudan.edu.cn>
2025-05-11 23:44:11 +0800

    feat: icon

--- src/types/images.ts
+++ src/types/images.ts
@@ -1,3 +1,6 @@
+const levels = ["D", "D+", "C", "C+", "B", "B+", "A", "A+", "S", "S+"];
+const icons = ["A", "C", "H", "L", "M"];
+const sequence = [0, 1, 2, 3, 4];
 export const ImageUrls = [
     "png/background.png",
     "png/calender.png",
@@ -6,4 +9,14 @@ export const ImageUrls = [
     "png/data-icon.png",
     "png/event-bg.png",
     "png/portrait-girl.png",
+    "/png/icon-h.png",
+    "/png/icon-l.png",
+    "/png/icon-a.png",
+    "/png/icon-c.png",
+    "/png/icon-m.png",
+    ...levels
+        .flatMap((level) =>
+            icons.map((icon) => `png/${level}/${icon}-${level}`),
+        )
+        .flatMap((levelPath) => sequence.map((i) => `${levelPath}-${i}.png`)),
 ];
