4ac7a470f111d72b25deeac0a21b772ed1d8dfdc
favouritetasty <19307110112@fudan.edu.cn>
2025-06-18 20:47:57 +0800

    feat: 二阶段事件音效

--- src/pages/after/hooks/useTapHandler.ts
+++ src/pages/after/hooks/useTapHandler.ts
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
 
+import { useAudio } from "@/hooks/useAudio";
 import { YearItem } from "@/stage2/stage2";
 
 export function useTapHandler(
     items: YearItem[],
     setDisplayItems: React.Dispatch<React.SetStateAction<YearItem[]>>,
 ) {
+    const { play: playTriggerEvent } = useAudio(
+        "audio/06 二阶段事件产生.wav",
+        1,
+    );
+
     useEffect(() => {
         let startX = 0;
         let startY = 0;
@@ -31,6 +37,7 @@ export function useTapHandler(
         const onPointerUp = (e: PointerEvent) => {
             if (e.pointerType !== "touch") return;
             if (!moved) {
+                playTriggerEvent();
                 setDisplayItems((prev) =>
                     prev.length < items.length
                         ? [...prev, items[prev.length]]
