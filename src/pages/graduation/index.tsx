e69d47ef1f0ffe7c36005f705b06b63711fb95ca
favouritetasty <19307110112@fudan.edu.cn>
2025-06-19 17:03:01 +0800

    feat: 调整 graduation

--- src/pages/graduation/index.tsx
+++ src/pages/graduation/index.tsx
@@ -1,8 +1,16 @@
-import { HTMLMotionProps } from "motion/react";
-import { FC, useRef } from "react";
+import {
+    animate,
+    HTMLMotionProps,
+    motion,
+    useMotionValue,
+    useTransform,
+} from "motion/react";
+import { FC, useEffect, useRef } from "react";
+import { twMerge } from "tailwind-merge";
 
 import { End } from "../end";
 
+import { IconSkip } from "@/assets";
 import Image from "@/components/image";
 import { StaggeredText } from "@/components/staggered-text";
 import { CircularTransitionTrigger } from "@/hooks/useCircularTransition";
@@ -25,14 +33,56 @@ const lines = [
     "感谢同学、同门、挚友们的青春共航",
     "感谢坚守在宿舍、食堂、门卫的叔叔阿姨",
     "更别忘记感谢一路坚持、从未放弃的自己",
+    "",
+    "毕业钟声已响",
+    "此去天高海阔",
+    "且从容，且峥嵘",
 ];
 
-// const lines2 = ["毕业钟声已响", "此去天高海阔", "且从容，且峥嵘"];
+const hat1Points = 15;
+const hat2Points = 17;
+const hat3Points = 18;
 
 export const Graduation: FC<GraduationProps> = ({ trigger }) => {
-    const eduDestination = $Data.use((state) => state.eduDestination);
     const gradDestination = $Data.use((state) => state.gradDestination);
     const triggerRef = useRef<NodeJS.Timeout>(undefined);
+
+    const x1 = useMotionValue(0);
+    const y1 = useTransform(
+        x1,
+        Array.from({ length: hat1Points }).map((_, i) => i * 12),
+        Array.from({ length: hat1Points }).map(
+            (_, i) => -(-i * i + 12 * i) * 2,
+        ),
+    );
+    const opacity1 = useTransform(x1, [0, 130, 140], [1, 1, 0]);
+    const x2 = useMotionValue(0);
+    const y2 = useTransform(
+        x2,
+        Array.from({ length: hat2Points }).map((_, i) => -i * 10),
+        Array.from({ length: hat2Points }).map(
+            (_, i) => -(-i * i + 18 * i) * 1.2,
+        ),
+    );
+    const opacity2 = useTransform(x2, [0, -150, -160], [1, 1, 0]);
+
+    const x3 = useMotionValue(0);
+    const y3 = useTransform(
+        x3,
+        Array.from({ length: hat3Points }).map((_, i) => -i * 10),
+        Array.from({ length: hat3Points }).map(
+            (_, i) => -(-i * i + 16 * i) * 1,
+        ),
+    );
+    const opacity3 = useTransform(x3, [0, -160, -170], [1, 1, 0]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            animate(x1, 140, { duration: 2, ease: "easeInOut" });
+            animate(x2, -160, { duration: 2, ease: "easeInOut" });
+            animate(x3, -170, { duration: 2, ease: "easeInOut" });
+        }, 9000);
+    }, []);
 
     if (gradDestination === "退学") {
         return <End dropout />;
@@ -45,15 +95,27 @@ export const Graduation: FC<GraduationProps> = ({ trigger }) => {
                 if (!triggerRef.current) {
                     triggerRef.current = setTimeout(() => {
                         trigger?.(e, "after");
-                    }, 2000);
+                    }, 0);
                 }
             }}
         >
-            <div className="absolute top-0 left-0 w-screen">
+            <div className="absolute top-[-3%] left-0 w-screen">
                 <Image src={getImagePath("end-header")} square={false} />
             </div>
+            <div className="absolute right-[2%] top-[1%] z-[50]">
+                <IconSkip
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        trigger?.(e, "after");
+                    }}
+                    className={twMerge(
+                        "text-4xl transition-colors",
+                        "text-[#7897B5]",
+                    )}
+                />
+            </div>
             <StaggeredText
-                className="absolute flex flex-col gap-px top-[17%] left-[7%] text-sm"
+                className="absolute flex flex-col w-[90vw] gap-px top-[12%] left-[50%] -translate-x-1/2"
                 lines={lines}
             />
             {/* <StaggeredText
@@ -62,12 +124,26 @@ export const Graduation: FC<GraduationProps> = ({ trigger }) => {
             /> */}
 
             <div className="absolute bottom-0 left-0 w-screen">
-                <Image src={getImagePath("end-footer")} square={false} />
-            </div>
-            <div className="absolute top-[58%] left-[50%]">
-                <h1>一阶段游戏结束力</h1>
-                <h1>学历: {eduDestination}</h1>
-                <h1>结局: {gradDestination}</h1>
+                <Image src={getImagePath("end-footer")} square={false}>
+                    <motion.div
+                        className="absolute bottom-[57%] right-[52%] w-[16vw]"
+                        style={{ x: x1, y: y1, opacity: opacity1 }}
+                    >
+                        <Image src={getImagePath("end-hat-1")} square={false} />
+                    </motion.div>
+                    <motion.div
+                        className="absolute bottom-[57%] right-[22%] w-[16vw]"
+                        style={{ x: x2, y: y2, opacity: opacity2 }}
+                    >
+                        <Image src={getImagePath("end-hat-2")} square={false} />
+                    </motion.div>
+                    <motion.div
+                        className="absolute bottom-[52%] right-[4%] w-[16vw]"
+                        style={{ x: x3, y: y3, opacity: opacity3 }}
+                    >
+                        <Image src={getImagePath("end-hat-3")} square={false} />
+                    </motion.div>
+                </Image>
             </div>
         </div>
     );
